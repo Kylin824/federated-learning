38f9bf4cee8f96afc59f85b34bc6ad3c515236b0
pc_gpu <493748267@qq.com>
2021-01-22 21:05:55 +0800

    test codes

--- main_fed.py
+++ main_fed.py
@@ -3,12 +3,13 @@
 # Python version: 3.6
 
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg') # 绘图不显示
 import matplotlib.pyplot as plt
 import copy
 import numpy as np
 from torchvision import datasets, transforms
 import torch
+import time
 
 from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
 from utils.options import args_parser
@@ -19,11 +20,13 @@ from models.test import test_img
 
 
 if __name__ == '__main__':
-    # parse args
+    # 读取参数
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
 
-    # load dataset and split users
+    print("cuda : ", torch.cuda.is_available())
+
+    # 加载数据，分配用户
     if args.dataset == 'mnist':
         trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
         dataset_train = datasets.MNIST('../data/mnist/', train=True, download=True, transform=trans_mnist)
@@ -47,21 +50,25 @@ if __name__ == '__main__':
 
     # build model
     if args.model == 'cnn' and args.dataset == 'cifar':
-        net_glob = CNNCifar(args=args).to(args.device)
+        global_net = CNNCifar(args=args).to(args.device)
     elif args.model == 'cnn' and args.dataset == 'mnist':
-        net_glob = CNNMnist(args=args).to(args.device)
+        global_net = CNNMnist(args=args).to(args.device)
     elif args.model == 'mlp':
         len_in = 1
         for x in img_size:
             len_in *= x
-        net_glob = MLP(dim_in=len_in, dim_hidden=200, dim_out=args.num_classes).to(args.device)
+        global_net = MLP(dim_in=len_in, dim_hidden=200, dim_out=args.num_classes).to(args.device)
     else:
         exit('Error: unrecognized model')
-    print(net_glob)
-    net_glob.train()
+
+    print(global_net)
+
+    global_net.train()
+
+    time_start = time.time()
 
     # copy weights
-    w_glob = net_glob.state_dict()
+    w_glob = global_net.state_dict()
 
     # training
     loss_train = []
@@ -74,41 +81,46 @@ if __name__ == '__main__':
     if args.all_clients: 
         print("Aggregation over all clients")
         w_locals = [w_glob for i in range(args.num_users)]
+
     for iter in range(args.epochs):
         loss_locals = []
         if not args.all_clients:
             w_locals = []
-        m = max(int(args.frac * args.num_users), 1)
-        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+        m = max(int(args.frac * args.num_users), 1) # default : m = 0.1 * 100 = 10
+        idxs_users = np.random.choice(range(args.num_users), m, replace=False) # 在num_users里面选m个
         for idx in idxs_users:
             local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
-            w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
+            w, loss = local.train(net=copy.deepcopy(global_net).to(args.device))
             if args.all_clients:
                 w_locals[idx] = copy.deepcopy(w)
             else:
                 w_locals.append(copy.deepcopy(w))
             loss_locals.append(copy.deepcopy(loss))
+
         # update global weights
         w_glob = FedAvg(w_locals)
 
         # copy weight to net_glob
-        net_glob.load_state_dict(w_glob)
+        global_net.load_state_dict(w_glob)
 
         # print loss
         loss_avg = sum(loss_locals) / len(loss_locals)
         print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
         loss_train.append(loss_avg)
 
+    time_end = time.time()
+    print('totally cost time: {:3f}s'.format(time_end - time_start))
+
     # plot loss curve
     plt.figure()
     plt.plot(range(len(loss_train)), loss_train)
     plt.ylabel('train_loss')
-    plt.savefig('./save/fed_{}_{}_{}_C{}_iid{}.png'.format(args.dataset, args.model, args.epochs, args.frac, args.iid))
+    plt.savefig('./save/fed_{}_{}_E{}_C{}_iid{}.png'.format(args.dataset, args.model, args.epochs, args.frac, args.iid))
 
     # testing
-    net_glob.eval()
-    acc_train, loss_train = test_img(net_glob, dataset_train, args)
-    acc_test, loss_test = test_img(net_glob, dataset_test, args)
+    global_net.eval()
+    acc_train, loss_train = test_img(global_net, dataset_train, args)
+    acc_test, loss_test = test_img(global_net, dataset_test, args)
     print("Training accuracy: {:.2f}".format(acc_train))
     print("Testing accuracy: {:.2f}".format(acc_test))
 
