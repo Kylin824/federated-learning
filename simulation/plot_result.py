87dc2cbf47e5c2512de79ed19981e3e5180cefc5
CalvinLin <31285778+Calvin24lky@users.noreply.github.com>
2021-02-05 15:46:43 +0800

    bug fix

--- simulation/plot_result.py
+++ simulation/plot_result.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-acc_random = np.loadtxt('acc_random_mnist_cnn_E200_C0.1_iidFalse.txt')
-acc_fedcs = np.loadtxt('acc_fedcs_mnist_cnn_E200_C0.1_iidFalse.txt')
-acc_ucb = np.loadtxt('acc_ucb_mnist_cnn_E200_C0.1_iidFalse.txt')
+acc_random = np.loadtxt('res/noniid_prob_0.8/acc_random_mnist_cnn_E200_C0.1_iidFalse.txt')
+acc_fedcs = np.loadtxt('res/noniid_prob_0.8/acc_fedcs_mnist_cnn_E200_C0.1_iidFalse.txt')
+acc_ucb = np.loadtxt('res/noniid_prob_0.8/acc_ucb_mnist_cnn_E200_C0.1_iidFalse.txt')
+# acc_linucb = np.loadtxt('res/noniid_prob_0.8/acc_linucb_mnist_cnn_E50_C0.1_iidFalse.txt')
 
 # plt.plot(range(len(acc_random)), acc_random)
 # plt.plot(range(len(acc_fedcs)), acc_fedcs)
@@ -12,9 +13,12 @@ acc_ucb = np.loadtxt('acc_ucb_mnist_cnn_E200_C0.1_iidFalse.txt')
 # plt.ylabel('Acc')
 # plt.show()
 
-plt.plot(range(100), acc_random[:100], label='random')
-plt.plot(range(100), acc_fedcs[:100], label='fedcs')
-plt.plot(range(100), acc_ucb[:100], label='ucb')
+round = range(50)
+
+plt.plot(round, acc_random[:50], label='random')
+plt.plot(round, acc_fedcs[:50], label='fedcs')
+plt.plot(round, acc_ucb[:50], label='ucb')
+# plt.plot(round, acc_linucb[:50], label='linucb')
 plt.xlabel('round')
 plt.ylabel('Acc')
 plt.legend()
